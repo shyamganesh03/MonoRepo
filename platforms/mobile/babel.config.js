ba30a69de17aeae28d6480a34d6459c81373383e
shyam <shyamganeshravichandran@gmail.com>
2024-02-11 18:17:42 +0530

    feat(monorepo): monorepo project initializes

---
+++ platforms/mobile/babel.config.js
@@ -0,0 +1,12 @@
+module.exports = (api) => {
+  const babelEnv = api.env()
+  const plugins = ['react-native-paper/babel']
+  // change to 'production' to check if this is working in 'development' mode
+  if (babelEnv !== 'development') {
+    plugins.push(['transform-remove-console', { exclude: ['error', 'warn'] }])
+  }
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins,
+  }
+}
