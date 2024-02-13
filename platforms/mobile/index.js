a4c1b2a289443695b9e3391247a4b30dcc596172
shyam <shyamganeshravichandran@gmail.com>
2024-02-13 23:09:08 +0530

    fix(mobile): mobile setup done

--- platforms/mobile/index.js
+++ platforms/mobile/index.js
@@ -3,7 +3,7 @@
  */
 
 import { AppRegistry } from 'react-native';
-import App from '@mono';
+import App from '@monorepo/shared/src/App';
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
