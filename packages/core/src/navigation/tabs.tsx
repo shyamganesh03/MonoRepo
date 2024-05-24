3c6a71f1e5addc7fdb83ee4d06085730904dbaf5
shyam <shyamganeshravichandran@gmail.com>
2024-05-24 11:55:59 +0530

    feat(Icons): Icons added

--- packages/core/src/navigation/tabs.tsx
+++ packages/core/src/navigation/tabs.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import Dashboard from '../screens/dashboard'
+import { Icon } from '@libs/native-icons'
+import { useTheme } from '@react-navigation/native'
 
 const Tab = createBottomTabNavigator()
 
 const Tabs = () => {
+  const { colors } = useTheme()
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Dashboard" component={Dashboard} />
+      <Tab.Screen
+        name="Dashboard"
+        component={Dashboard}
+        options={{
+          tabBarIcon: () => <Icon name="HomeIcon" color={colors.primary} />,
+        }}
+      />
     </Tab.Navigator>
   )
 }
